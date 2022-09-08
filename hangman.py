a68a03947d7233fca9f7c9a99ebfeaec7aafab0a
Loryn <lorynhaykus@yahoo.com>
2022-09-07 20:10:44 -0600

    creating funciton to keep track of what letters have been guessed and how many letters are already in the word

--- hangman.py
+++ hangman.py
@@ -1,6 +1,7 @@
 #import the array 'words' and import the data
 from words import words
 import random
+import string
 
 #define a function to get valid words without extra characters in them
 def valid_word(words):
@@ -14,3 +15,9 @@ def valid_word(words):
     #return the word in uppercase
     return word.upper()
 
+# funciton to keep track of which letters have been guessed and which are correctly guessed 
+def hangman():
+    word = valid_word(words)
+    word_letters = set(word) #counting the letters in the word to keep tracked of which ones have been guessed
+    alphabet = set(string.ascii_uppercase) #calling predetermined uppercase letters in the english alpahbet
+    used_letters = set() #keeping track of what letters the user has already guessed 
